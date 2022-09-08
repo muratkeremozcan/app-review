5efa2a52013bb620d9745104bc41f7658307e898
Murat Ozcan <muratkerem@gmail.com>
2022-09-08 10:04:43 -0500

    useAxios hook

--- src/heroes/Heroes.tsx
+++ src/heroes/Heroes.tsx
@@ -2,39 +2,13 @@ import {useNavigate, Routes, Route} from 'react-router-dom'
 import ListHeader from 'components/ListHeader'
 import ModalYesNo from 'components/ModalYesNo'
 import HeroList from './HeroList'
-import {useCallback, useEffect, useState} from 'react'
+import {useState} from 'react'
 import HeroDetail from './HeroDetail'
-import axios, {AxiosResponse} from 'axios'
+import useAxios from 'hooks/useAxios'
 
 export default function Heroes() {
   const [showModal, setShowModal] = useState<boolean>(false)
-  const [heroes, setHeroes] = useState([])
-
-  // TODO: identify a better type later
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const parseList = (response: AxiosResponse<any>) => {
-    if (response.status !== 200) throw Error(response.statusText)
-    let list = response.data
-    if (typeof list !== 'object') {
-      list = []
-    }
-    console.log(list)
-    return list
-  }
-
-  const getData = useCallback(async () => {
-    const response = await axios.get(`${process.env.REACT_APP_API_URL}/heroes`)
-    return parseList(response)
-  }, [])
-
-  useEffect(() => {
-    console.log('mounting')
-    getData().then(data => {
-      setHeroes(data)
-    })
-
-    return () => console.log('unmounting')
-  }, []) // empty array to have the effect occur only once
+  const {data: heroes = []} = useAxios('heroes')
 
   const navigate = useNavigate()
   const addNewHero = () => navigate('/heroes/add-hero')
